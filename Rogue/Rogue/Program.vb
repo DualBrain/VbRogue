d73773a5073f9a877e1c7171ca6e6293a782ac8a
Cory Smith <corysmith@addressof.com>
2019-06-11 22:09:55 -0500

    Working through setting up the console to support "game play".  Modified palette.  Added ASCII lookup to make things easier.  Prototyping drawing "stuff" to the screen.  Spent some time adding "things".

--- Rogue/Rogue/Program.vb
+++ Rogue/Rogue/Program.vb
@@ -1,7 +1,282 @@
-'Imports System
+﻿Option Explicit On
+Option Strict On
+Option Infer On
+
+Imports System.Console
+Imports System.Text
+Imports ConsoleEx
+Imports QB
 
 Module Program
+
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
+    ' The following is from a sample by Lucian... 
+    ' so I'll take it as the gospel. ;-)
+    MainAsync(args).GetAwaiter().GetResult()
   End Sub
+
+  Private ReadOnly TUNNEL As Char = QBChar(178)
+  Private ReadOnly WALL_TOP_LEFT As Char = QBChar(201)
+  Private ReadOnly WALL_HORIZONTAL As Char = QBChar(205)
+  'Private ReadOnly WALL_TOP_CENTER As Char = QBChar(203)
+  Private ReadOnly WALL_TOP_RIGHT As Char = QBChar(187)
+  Private ReadOnly WALL_DOOR As Char = QBChar(206)
+  Private ReadOnly WALL_BOTTOM_LEFT As Char = QBChar(200)
+  'Private ReadOnly WALL_BOTTOM_CENTER As Char = QBChar(202)
+  Private ReadOnly WALL_BOTTOM_RIGHT As Char = QBChar(188)
+  Private ReadOnly WALL_VERTICAL As Char = QBChar(186)
+  Private ReadOnly FLOOR As Char = QBChar(250)
+
+  Private Enum Objects
+    Void = 0
+
+    Floor = 1
+    Tunnel = 2
+    Door = 3
+    Stairs = 10
+    Trap = 11
+
+    WallTopLeft = 4
+    WallTopRight = 5
+    WallHorizontal = 6
+    WallVertical = 7
+    WallBottomLeft = 8
+    WallBottomRight = 9
+
+    Character = 20
+
+    Bat = 35
+    Emu = 34
+    Hobgoblin = 33
+    Imp = 30
+    Kestral = 31
+    Orc = 36
+    RattleSnake = 37
+    Snake = 32
+    Zombie = 38
+
+    Gold = 50
+
+    Potion = 51
+    ClubQuestionMark = 52
+    Ring = 53
+    Armor = 54
+    Scroll = 55
+    Weapon = 56
+    Food = 57
+    Staff = 58
+    Amulet = 59
+    SafeMagic = 60 ' $
+    PerilousMagic = 61 ' +
+
+  End Enum
+
+  Private Async Function MainAsync(args As String()) As Task
+
+#Region "Configure Console Window"
+
+    Title = "ROGUE"
+
+    Dim OrgBufferHeight%, OrgBufferWidth%, OrgWindowHeight%, OrgWindowWidth%
+
+    OrgBufferHeight = BufferHeight
+    OrgBufferWidth = BufferWidth
+    OrgWindowHeight = WindowHeight
+    OrgWindowWidth = WindowWidth
+
+    OutputEncoding = Encoding.UTF8
+    Resize(80, 26)
+    DisableMinimize()
+    DisableMaximize()
+    DisableResize()
+    DisableQuickEditMode()
+
+    CursorVisible = False
+
+    ' MS-DOS Dark Yellow is more of a Brown.
+    ConsoleEx.SetColor(ConsoleColor.DarkYellow, 170, 85, 0)
+    ConsoleEx.SetColor(ConsoleColor.DarkMagenta, 85, 85, 255)
+
+    Try
+
+#End Region
+
+      Dim C = Objects.Character
+      Dim K = Objects.Kestral
+      Dim I = Objects.Imp
+      Dim G = Objects.Gold
+      Dim P = Objects.Potion
+      Dim L = Objects.ClubQuestionMark
+      Dim S = Objects.Snake
+      Dim H = Objects.Hobgoblin
+      Dim R = Objects.Ring
+      Dim D = Objects.Stairs
+      Dim A = Objects.Armor
+      Dim T = Objects.Trap
+
+      Dim room = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 6, 6, 6, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, D, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 6, 6, 3, 6, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 4, 6, 6, 6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 7, 1, 1, C, K, 1, 1, 1, 1, 1, I, 1, 1, 1, 1, 1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                  {0, 0, 0, 7, 1, 1, 1, 1, 1, 1, 1, A, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 7, T, 1, 1, 1, 1, P, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 7, 1, 1, 1, L, G, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}
+
+      Dim stairCol = -1
+      Dim stairRow = -1
+
+      For row = 0 To room.GetUpperBound(0) - 1
+        For col = 0 To room.GetUpperBound(1) - 1
+          SetCursorPosition(col, row)
+          Select Case CType(room(row, col), Objects)
+
+            Case Objects.Trap
+              ForegroundColor = ConsoleColor.Magenta
+              Write(QBChar(4))
+            Case Objects.Stairs
+              ForegroundColor = ConsoleColor.Black
+              BackgroundColor = ConsoleColor.Green
+              Write(" ")
+              stairCol = col
+              stairRow = row
+              ResetColor()
+            Case Objects.Character
+              ForegroundColor = ConsoleColor.Yellow
+              Write(QBChar(1))
+            Case Objects.Gold
+              ForegroundColor = ConsoleColor.Yellow
+              Write(QBChar(15))
+            Case Objects.ClubQuestionMark
+              ForegroundColor = ConsoleColor.Red
+              Write(QBChar(5))
+            Case Objects.Armor
+              ForegroundColor = ConsoleColor.DarkMagenta
+              Write(QBChar(8))
+            Case Objects.Potion
+              ForegroundColor = ConsoleColor.DarkMagenta
+              Write(QBChar(173))
+            Case Objects.Scroll
+              ForegroundColor = ConsoleColor.DarkMagenta
+              Write(QBChar(13))
+            Case Objects.Ring
+              ForegroundColor = ConsoleColor.DarkMagenta
+              Write(QBChar(248)) ' Might be a 9
+
+            Case Objects.Snake
+              ForegroundColor = ConsoleColor.DarkGray
+              Write("S")
+            Case Objects.Hobgoblin
+              ForegroundColor = ConsoleColor.DarkGray
+              Write("H")
+            Case Objects.Kestral
+              ForegroundColor = ConsoleColor.DarkGray
+              Write("K")
+            Case Objects.Imp
+              ForegroundColor = ConsoleColor.DarkGray
+              Write("I")
+
+            Case Objects.Void
+              ForegroundColor = ConsoleColor.Black
+              Write(" ")
+            Case Objects.Tunnel
+              ForegroundColor = ConsoleColor.DarkGray
+              Write(TUNNEL)
+            Case Objects.Floor
+              ForegroundColor = ConsoleColor.DarkGreen
+              Write(FLOOR)
+            Case Objects.WallTopLeft
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_TOP_LEFT)
+            Case Objects.WallTopRight
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_TOP_RIGHT)
+            Case Objects.WallHorizontal
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_HORIZONTAL)
+            Case Objects.WallVertical
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_VERTICAL)
+            Case Objects.WallBottomLeft
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_BOTTOM_LEFT)
+            Case Objects.WallBottomRight
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_BOTTOM_RIGHT)
+            Case Objects.Door
+              ForegroundColor = ConsoleColor.DarkYellow
+              Write(WALL_DOOR)
+          End Select
+        Next
+      Next
+
+      Dim alt = True
+
+      Do
+
+        Await Task.Delay(200)
+
+        If stairRow > -1 AndAlso stairCol > -1 Then
+          ' Draw stairway...
+          ForegroundColor = ConsoleColor.Black
+          BackgroundColor = ConsoleColor.Green
+          SetCursorPosition(staircol, stairrow)
+          If alt Then
+            Write(" ")
+          Else
+            Write(QBChar(240))
+          End If
+          ResetColor()
+          alt = Not alt
+        End If
+
+        If KeyAvailable Then
+          Dim key = ReadKey(True)
+          If key.Key = ConsoleKey.Spacebar Then
+            Exit Do
+          End If
+        End If
+
+      Loop
+
+#Region "Reset Console Window"
+
+    Finally
+
+      EnableMinimize()
+      EnableMaximize()
+      EnableResize()
+      ForegroundColor = ConsoleColor.Gray
+      BackgroundColor = ConsoleColor.Black
+
+      SetBufferSize(OrgBufferWidth, OrgBufferHeight)
+      SetWindowSize(OrgWindowWidth, OrgWindowHeight)
+
+      EnableQuickEditMode()
+
+      ResetColor()
+      'CLS()
+
+      CursorVisible = True
+
+      If Debugger.IsAttached Then
+        WriteLine()
+        WriteLine("Press enter to continue...")
+        ReadLine()
+      End If
+
+    End Try
+
+#End Region
+
+  End Function
+
 End Module
---
+++ Rogue/Rogue/QB/QB.vb
@@ -0,0 +1,201 @@
+﻿Option Explicit On
+Option Strict On
+Option Infer On
+
+Namespace Global
+
+  Public NotInheritable Class QB
+
+    Private Sub New()
+    End Sub
+
+    Public Shared Function QBChr(value%) As String
+      Return QBChar(value).ToString
+    End Function
+
+    Public Shared Function QBChar(value%) As Char
+
+      If value > -1 AndAlso value < 256 Then
+
+        Select Case value
+          Case 0 : Return " "c
+          Case 1 : Return "☺"c
+          Case 2 : Return "☻"c
+          Case 3 : Return "♥"c
+          Case 4 : Return "♦"c
+          Case 5 : Return "♣"c
+          Case 6 : Return "♠"c
+          Case 7 : Return "●"c
+          Case 8 : Return "◘"c
+          Case 9 : Return "○"c
+          Case 10 : Return "◙"c
+          Case 11 : Return "♂"c
+          Case 12 : Return "♀"c
+          Case 13 : Return "♪"c
+          Case 14 : Return "♫"c
+          Case 15 : Return "✶"c
+          Case 16 : Return "►"c
+          Case 17 : Return "◄"c
+          Case 18 : Return "↕"c
+          Case 19 : Return "‼"c
+          Case 20 : Return "¶"c
+          Case 21 : Return "§"c
+          Case 22 : Return "▬"c
+          Case 23 : Return "↨"c
+          Case 24 : Return "↑"c
+          Case 25 : Return "↓"c
+          Case 26 : Return "→"c
+          Case 27 : Return "←"c
+          Case 28 : Return "՟"c
+          Case 29 : Return "↔"c
+          Case 30 : Return "▲"c
+          Case 31 : Return "▼"c
+          Case 32 : Return " "c
+          Case 32 To 125
+            Return ChrW(value)
+          Case 126 : Return "~"c
+          Case 127 : Return "Δ"c
+          Case 128 : Return "Ç"c
+          Case 129 : Return "ü"c
+          Case 130 : Return "é"c
+          Case 131 : Return "â"c
+          Case 132 : Return "ä"c
+          Case 133 : Return "à"c
+          Case 134 : Return "å"c
+          Case 135 : Return "ç"c
+          Case 136 : Return "ê"c
+          Case 137 : Return "ë"c
+          Case 138 : Return "è"c
+          Case 139 : Return "ï"c
+          Case 140 : Return "î"c
+          Case 141 : Return "ì"c
+          Case 142 : Return "Ä"c
+          Case 143 : Return "Å"c
+          Case 144 : Return "É"c
+          Case 145 : Return "æ"c
+          Case 146 : Return "Æ"c
+          Case 147 : Return "ô"c
+          Case 148 : Return "ö"c
+          Case 149 : Return "ò"c
+          Case 150 : Return "û"c
+          Case 151 : Return "ù"c
+          Case 152 : Return "ÿ"c
+          Case 153 : Return "Ö"c
+          Case 154 : Return "Ü"c
+          Case 155 : Return "¢"c
+          Case 156 : Return "£"c
+          Case 157 : Return "¥"c
+          Case 158 : Return "₧"c
+          Case 159 : Return "ƒ"c
+          Case 160 : Return "á"c
+          Case 161 : Return "í"c
+          Case 162 : Return "ó"c
+          Case 163 : Return "ú"c
+          Case 164 : Return "ñ"c
+          Case 165 : Return "Ñ"c
+          Case 166 : Return "ª"c
+          Case 167 : Return "º"c
+          Case 168 : Return "¿"c
+          Case 169 : Return "⌐"c
+          Case 170 : Return "¬"c
+          Case 171 : Return "½"c
+          Case 172 : Return "¼"c
+          Case 173 : Return "¡"c
+          Case 174 : Return "«"c
+          Case 175 : Return "»"c
+          Case 176 : Return "░"c
+          Case 177 : Return "▒"c
+          Case 178 : Return "▓"c
+          Case 179 : Return "│"c
+          Case 180 : Return "┤"c
+          Case 181 : Return "╡"c
+          Case 182 : Return "╢"c
+          Case 183 : Return "╖"c
+          Case 184 : Return "╕"c
+          Case 185 : Return "╣"c
+          Case 186 : Return "║"c
+          Case 187 : Return "╗"c
+          Case 188 : Return "╝"c
+          Case 189 : Return "╜"c
+          Case 190 : Return "╛"c
+          Case 191 : Return "┐"c
+          Case 192 : Return "└"c
+          Case 193 : Return "┴"c
+          Case 194 : Return "┬"c
+          Case 195 : Return "├"c
+          Case 196 : Return "─"c
+          Case 197 : Return "┼"c
+          Case 198 : Return "╞"c
+          Case 199 : Return "╟"c
+          Case 200 : Return "╚"c
+          Case 201 : Return "╔"c
+          Case 202 : Return "╩"c
+          Case 203 : Return "╦"c
+          Case 204 : Return "╠"c
+          Case 205 : Return "═"c
+          Case 206 : Return "╬"c
+          Case 207 : Return "╧"c
+          Case 208 : Return "╨"c
+          Case 209 : Return "╤"c
+          Case 210 : Return "╥"c
+          Case 211 : Return "╙"c
+          Case 212 : Return "╘"c
+          Case 213 : Return "╒"c
+          Case 214 : Return "╓"c
+          Case 215 : Return "╫"c
+          Case 216 : Return "╪"c
+          Case 217 : Return "┘"c
+          Case 218 : Return "┌"c
+          Case 219 : Return " "c ' ????
+          Case 220 : Return "▄"c
+          Case 221 : Return "▌"c
+          Case 222 : Return "▐"c
+          Case 223 : Return "▀"c
+          Case 224 : Return "α"c
+          Case 225 : Return "ß"c
+          Case 226 : Return "Γ"c
+          Case 227 : Return "π"c
+          Case 228 : Return "Σ"c
+          Case 229 : Return "σ"c
+          Case 230 : Return "µ"c
+          Case 231 : Return "τ"c
+          Case 232 : Return "Φ"c
+          Case 233 : Return "Θ"c
+          Case 234 : Return "Ω"c
+          Case 235 : Return "δ"c
+          Case 236 : Return "∞"c
+          Case 237 : Return "φ"c
+          Case 238 : Return "ε"c
+          Case 239 : Return "∩"c
+          Case 240 : Return "≡"c
+          Case 241 : Return "±"c
+          Case 242 : Return "≥"c
+          Case 243 : Return "≤"c
+          Case 244 : Return "⌠"c
+          Case 245 : Return "⌡"c
+          Case 246 : Return "÷"c
+          Case 247 : Return "≈"c
+          Case 248 : Return "°"c
+          Case 249 : Return "∙"c
+          Case 250 : Return "·"c
+          Case 251 : Return "√"c
+          Case 252 : Return "ⁿ"c
+          Case 253 : Return "²"c
+          Case 254 : Return "█"c
+          Case 255 : Return " "c
+
+          Case 256 : Return "■"c
+
+          Case Else
+            Return " "c
+        End Select
+
+      Else
+        Throw New ArgumentOutOfRangeException("value")
+      End If
+
+    End Function
+
+  End Class
+
+End Namespace
