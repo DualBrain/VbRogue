2437af3966ec90f64e7a2ed31b69cb26d7abdcde
Cory Smith <corysmith@addressof.com>
2019-06-18 21:54:19 -0500

    Made a ton of changes... map is "predetermined" (but can be drawn using simple characters on the keyboard).  Simple fog of war.  Basic movement implemented.  Discovering of where the exit is located is working.

---
+++ Rogue/Rogue/GameData.vb
@@ -0,0 +1,138 @@
+﻿Module GameData
+
+  Friend Structure Monster
+
+    Public Sub New(symbol As Char,
+                   name As String,
+                   treasurePercent As Integer,
+                   experience As Integer,
+                   hitDie As Integer,
+                   armorClass As Integer,
+                   damage As String,
+                   notes As String)
+
+      Me.Symbol = symbol
+      Me.Name = name
+      Me.TreasurePercent = treasurePercent
+      Me.Experience = experience
+      Me.HitDie = hitDie
+      Me.ArmorClass = armorClass
+      Me.Damage = damage
+      Me.Notes = notes
+
+    End Sub
+
+    Public Symbol As Char
+    Public Name As String
+    Public TreasurePercent As Integer
+    Public Experience As Integer
+    Public HitDie As Integer
+    Public ArmorClass As Integer
+    Public Damage As String
+    Public Notes As String
+
+    Public IsMean As Boolean
+    Public IsFlying As Boolean
+    Public IsRegerating As Boolean
+    Public IsGreedy As Boolean
+    Public IsInvisible As Boolean
+
+  End Structure
+
+  Public Structure Damage
+
+    Public Sub New(count%, type%)
+      Me.DieCount = count
+      Me.DieType = type
+    End Sub
+
+    Public DieCount As Integer
+    Public DieType As Integer
+
+  End Structure
+
+  Public Structure Weapon
+
+    Sub New(name$,
+            melee As Damage,
+            thrown As Damage)
+      Me.Name = name
+      Me.MeleeDamage = melee
+      Me.ThrownDamage = thrown
+    End Sub
+
+    Public Name As String
+    Public MeleeDamage As Damage
+    Public ThrownDamage As Damage
+
+  End Structure
+
+  Friend Structure Armor
+
+    Public Sub New(name$, armorClass%)
+      Me.Name = name
+      Me.ArmorClass = armorClass
+    End Sub
+
+    Public Name As String
+    Public ArmorClass As Integer
+
+  End Structure
+
+  ' Scrolls
+  ' Potions
+  ' Rings
+  ' Rods
+
+  ' Traps
+
+  ReadOnly armors As New List(Of Armor) From
+    {New Armor("Leather", 3),
+     New Armor("Ring Mail", 4),
+     New Armor("Studded Leather", 4),
+     New Armor("Scale Mail", 5),
+     New Armor("Chain Mail", 6),
+     New Armor("Splint Mail", 7),
+     New Armor("Banded Mail", 7),
+     New Armor("Plate Mail", 8)}
+
+  ReadOnly weapons As New List(Of Weapon) From
+    {New Weapon("Mace", New Damage(2, 4), Nothing),
+     New Weapon("Long Sword", New Damage(3, 4), Nothing),
+     New Weapon("Short Bow", New Damage(1, 1), Nothing),
+     New Weapon("Arrow", New Damage(2, 3), Nothing),
+     New Weapon("Dagger", New Damage(1, 6), New Damage(1, 4)),
+     New Weapon("Two-handed Sword", New Damage(4, 4), Nothing),
+     New Weapon("Dart", Nothing, New Damage(1, 3)),
+     New Weapon("Shuriken", Nothing, New Damage(2, 4)),
+     New Weapon("Spear", New Damage(2, 3), New Damage(1, 6))}
+
+  ReadOnly monsters As New List(Of Monster) From
+    {New Monster("A"c, "Aquator", 0, 20, 5, 2, "0d0/0d0", "Rusts armor") With {.IsMean = True},
+     New Monster("B"c, "Bat", 0, 1, 1, 3, "1d2", "Flies Randomly") With {.IsFlying = True},
+     New Monster("C"c, "Centaur", 15, 17, 4, 4, "1d2/1d5/1d5", Nothing),
+     New Monster("D"c, "Dragon", 100, 5000, 10, -1, "1d8/1d8/3d10", "Ranged 6d6 flame attack") With {.IsMean = True},
+     New Monster("E"c, "Emu", 0, 2, 1, 7, "1d2", Nothing) With {.IsMean = True},
+     New Monster("F"c, "Venus Flytrap", 0, 80, 8, 3, "special", "Traps Player") With {.IsMean = True},
+     New Monster("G"c, "Griffin", 20, 2000, 13, 2, "4d3/3d5", Nothing) With {.IsMean = True, .IsFlying = True, .IsRegerating = True},
+     New Monster("H"c, "Hobgoblin", 0, 3, 1, 5, "1d8", Nothing) With {.IsMean = True},
+     New Monster("I"c, "Ice Monster", 0, 5, 1, 9, "0d0", "Freezes Player"),
+     New Monster("J"c, "Jabberwock", 70, 3000, 15, 6, "2d12/2d4", Nothing),
+     New Monster("K"c, "Kestrel", 0, 1, 1, 7, "1d4", Nothing) With {.IsMean = True, .IsFlying = True},
+     New Monster("L"c, "Leprechaun", 0, 10, 3, 8, "1d1", "Steels Gold"),
+     New Monster("M"c, "Medusa", 40, 200, 8, 2, "3d4/3d4/2d5", "Confuses Player") With {.IsMean = True},
+     New Monster("N"c, "Nymph", 100, 37, 3, 9, "0d0", "Steels Magic Item"),
+     New Monster("O"c, "Orc", 15, 5, 1, 6, "1d8", "Greedy - Runs toward gold") With {.IsGreedy = True},
+     New Monster("P"c, "Phantom", 0, 120, 8, 3, "4d4", "Invisible"),
+     New Monster("Q"c, "Quagga", 0, 15, 3, 3, "1d5/1d5", Nothing) With {.IsInvisible = True},
+     New Monster("R"c, "Rattlesnake", 0, 9, 2, 3, "1d6", "Reduces Strength") With {.IsMean = True},
+     New Monster("S"c, "Snake", 0, 2, 1, 5, "1d3", Nothing) With {.IsMean = True},
+     New Monster("T"c, "Troll", 50, 120, 6, 4, "1d8/1d8/2d6", Nothing) With {.IsMean = True, .IsRegerating = True},
+     New Monster("U"c, "Ur-vile", 0, 190, 7, -1, "1d9/1d9/2d9", Nothing) With {.IsMean = True},
+     New Monster("V"c, "Vampire", 20, 350, 8, 1, "1d10", "Drains Max HP") With {.IsMean = True, .IsRegerating = True},
+     New Monster("W"c, "Wraith", 0, 55, 5, 4, "1d6", "Drains Exp"),
+     New Monster("X"c, "Xeroc", 30, 100, 7, 7, "4d4", "Imitates an object"),
+     New Monster("Y"c, "Yeti", 30, 50, 4, 6, "1d6/1d6", Nothing),
+     New Monster("Z"c, "Zombie", 0, 6, 2, 8, "1d8", Nothing) With {.IsMean = True}}
+
+End Module
--- Rogue/Rogue/Program.vb
+++ Rogue/Rogue/Program.vb
@@ -9,11 +9,11 @@ Imports QB
 
 Module Program
 
-  Sub Main(args As String())
-    ' The following is from a sample by Lucian... 
-    ' so I'll take it as the gospel. ;-)
-    MainAsync(args).GetAwaiter().GetResult()
-  End Sub
+  'Sub Main(args As String())
+  '  ' The following is from a sample by Lucian... 
+  '  ' so I'll take it as the gospel. ;-)
+  '  MainAsync(args).GetAwaiter().GetResult()
+  'End Sub
 
   Private ReadOnly TUNNEL As Char = QBChar(178)
   Private ReadOnly WALL_TOP_LEFT As Char = QBChar(201)
@@ -58,7 +58,6 @@ Module Program
     Gold = 50
 
     Potion = 51
-    ClubQuestionMark = 52
     Ring = 53
     Armor = 54
     Scroll = 55
@@ -71,7 +70,8 @@ Module Program
 
   End Enum
 
-  Private Async Function MainAsync(args As String()) As Task
+  'Private Async Function MainAsync(args As String()) As Task
+  Public Sub Main(args As String())
 
 #Region "Configure Console Window"
 
@@ -101,134 +101,86 @@ Module Program
 
 #End Region
 
-      Dim C = Objects.Character
-      Dim K = Objects.Kestral
-      Dim I = Objects.Imp
-      Dim G = Objects.Gold
-      Dim P = Objects.Potion
-      Dim L = Objects.ClubQuestionMark
-      Dim S = Objects.Snake
-      Dim H = Objects.Hobgoblin
-      Dim R = Objects.Ring
-      Dim D = Objects.Stairs
-      Dim A = Objects.Armor
-      Dim T = Objects.Trap
+      Dim ruler = "
+         1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+      Dim map = "
+             [----]                                             [--------]      
+             |....|                                             |........|      
+             |....|                                     ########+........|      
+             |....|                                     #       |........|      
+             |....+###################                  #       {--------}      
+             {--+-}                  ####################                       
+        #########                      ############                             
+   [----+----------]                  [+----]                                   
+   |...............|                  |.....|      ################             
+   |..@............+##################+.....|      #              #             
+   |...............|                  |.....|      #              #             
+   |...............|                  |.....+#######              #             
+   |...............|                  |.....|                     #             
+   {---------------}                  {+----}                     #             
+                                ########                   ########             
+                              [-+]                         #                    
+                              |..+##########               #                    
+                         #####+..|         #          [----+--------------]     
+                              |..|         ###########+...................|     
+                              |..|                    |...........=.......|     
+                              {--}                    {-------------------}     
+"
 
-      Dim room = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 6, 6, 6, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, D, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 1, 1, 1, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 6, 6, 3, 6, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 4, 6, 6, 6, 6, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 7, 1, 1, C, K, 1, 1, 1, 1, 1, I, 1, 1, 1, 1, 1, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-                  {0, 0, 0, 7, 1, 1, 1, 1, 1, 1, 1, A, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 7, T, 1, 1, 1, 1, P, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 7, 1, 1, 1, L, G, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 8, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}
+      ' Convert the map to what we want to display...
 
-      Dim stairCol = -1
-      Dim stairRow = -1
+      Dim temp = Split(map, vbCrLf)
+      Dim room(temp.Length - 3) As String ' Look for a cleaner way to handle this.
+      For index = 0 To 20
+        room(index) = temp(index + 1)
+      Next
 
-      For row = 0 To room.GetUpperBound(0) - 1
-        For col = 0 To room.GetUpperBound(1) - 1
-          SetCursorPosition(col, row)
-          Select Case CType(room(row, col), Objects)
+      ' Holds the current location of the "stairs" (aka hole).
+      Dim stairX = -1
+      Dim stairY = -1
+      Dim stairFound = False
 
-            Case Objects.Trap
-              ForegroundColor = ConsoleColor.Magenta
-              Write(QBChar(4))
-            Case Objects.Stairs
-              ForegroundColor = ConsoleColor.Black
-              BackgroundColor = ConsoleColor.Green
-              Write(" ")
-              stairCol = col
-              stairRow = row
-              ResetColor()
-            Case Objects.Character
-              ForegroundColor = ConsoleColor.Yellow
-              Write(QBChar(1))
-            Case Objects.Gold
-              ForegroundColor = ConsoleColor.Yellow
-              Write(QBChar(15))
-            Case Objects.ClubQuestionMark
-              ForegroundColor = ConsoleColor.Red
-              Write(QBChar(5))
-            Case Objects.Armor
-              ForegroundColor = ConsoleColor.DarkMagenta
-              Write(QBChar(8))
-            Case Objects.Potion
-              ForegroundColor = ConsoleColor.DarkMagenta
-              Write(QBChar(173))
-            Case Objects.Scroll
-              ForegroundColor = ConsoleColor.DarkMagenta
-              Write(QBChar(13))
-            Case Objects.Ring
-              ForegroundColor = ConsoleColor.DarkMagenta
-              Write(QBChar(248)) ' Might be a 9
+      Dim heroX = -1
+      Dim heroY = -1
 
-            Case Objects.Snake
-              ForegroundColor = ConsoleColor.DarkGray
-              Write("S")
-            Case Objects.Hobgoblin
-              ForegroundColor = ConsoleColor.DarkGray
-              Write("H")
-            Case Objects.Kestral
-              ForegroundColor = ConsoleColor.DarkGray
-              Write("K")
-            Case Objects.Imp
-              ForegroundColor = ConsoleColor.DarkGray
-              Write("I")
-
-            Case Objects.Void
-              ForegroundColor = ConsoleColor.Black
-              Write(" ")
-            Case Objects.Tunnel
-              ForegroundColor = ConsoleColor.DarkGray
-              Write(TUNNEL)
-            Case Objects.Floor
-              ForegroundColor = ConsoleColor.DarkGreen
-              Write(FLOOR)
-            Case Objects.WallTopLeft
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_TOP_LEFT)
-            Case Objects.WallTopRight
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_TOP_RIGHT)
-            Case Objects.WallHorizontal
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_HORIZONTAL)
-            Case Objects.WallVertical
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_VERTICAL)
-            Case Objects.WallBottomLeft
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_BOTTOM_LEFT)
-            Case Objects.WallBottomRight
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_BOTTOM_RIGHT)
-            Case Objects.Door
-              ForegroundColor = ConsoleColor.DarkYellow
-              Write(WALL_DOOR)
+      Clear() ' Clear the screen.
+      WriteLine() ' Start with a blank line...
+      For y = 0 To 20
+        For x = 0 To 79
+          Dim c = room(y)(x)
+          Select Case c
+            Case "="c : stairX = x : stairY = y
+            Case "@"c
+              heroX = x : heroY = y
+              mid(room(y), x + 1, 1) = "."c
+            Case Else
           End Select
+          'DrawRoomTile(room, x, y)
         Next
       Next
 
+      PlaceHero(room, heroX, heroY, stairFound)
+
       Dim alt = True
+
+      Dim cycles = 0
 
       Do
 
-        Await Task.Delay(200)
+        'Await Task.Delay(1)
 
-        If stairRow > -1 AndAlso stairCol > -1 Then
+        Threading.Thread.Sleep(1)
+
+        If stairFound AndAlso
+           cycles > 250 AndAlso
+           (stairY > -1 AndAlso stairX > -1) AndAlso
+           Not (heroY = stairY AndAlso heroX = stairX) Then
+          cycles = 0
           ' Draw stairway...
           ForegroundColor = ConsoleColor.Black
           BackgroundColor = ConsoleColor.Green
-          SetCursorPosition(staircol, stairrow)
+          SetCursorPosition(stairX, stairY + 1)
           If alt Then
             Write(" ")
           Else
@@ -240,10 +192,38 @@ Module Program
 
         If KeyAvailable Then
           Dim key = ReadKey(True)
-          If key.Key = ConsoleKey.Spacebar Then
-            Exit Do
-          End If
+          Select Case key.Key
+            Case ConsoleKey.UpArrow
+              If CanMove(room, heroX, heroY - 1) Then
+                DrawRoomTile(room, heroX, heroY)
+                heroY -= 1
+                PlaceHero(room, heroX, heroY, stairFound)
+              End If
+            Case ConsoleKey.DownArrow
+              If CanMove(room, heroX, heroY + 1) Then
+                DrawRoomTile(room, heroX, heroY)
+                heroY += 1
+                PlaceHero(room, heroX, heroY, stairFound)
+              End If
+            Case ConsoleKey.LeftArrow
+              If CanMove(room, heroX - 1, heroY) Then
+                DrawRoomTile(room, heroX, heroY)
+                heroX -= 1
+                PlaceHero(room, heroX, heroY, stairFound)
+              End If
+            Case ConsoleKey.RightArrow
+              If CanMove(room, heroX + 1, heroY) Then
+                DrawRoomTile(room, heroX, heroY)
+                heroX += 1
+                PlaceHero(room, heroX, heroY, stairFound)
+              End If
+            Case ConsoleKey.Spacebar, ConsoleKey.Escape
+              Exit Do
+            Case Else
+          End Select
         End If
+
+        cycles += 1
 
       Loop
 
@@ -277,6 +257,70 @@ Module Program
 
 #End Region
 
+  End Sub
+
+  Private Function CanMove(room$(), x%, y%) As Boolean
+    Dim c = room(y)(x)
+    Select Case c
+      Case "."c, "+"c, "#"c, "="c
+        Return True
+      Case Else
+        Return False
+    End Select
+  End Function
+
+  Private Sub PlaceHero(room$(), x%, y%, ByRef stairFound As Boolean)
+    For yy = -1 To 1
+      For xx = -1 To 1
+        Dim foundStairs = DrawRoomTile(room$, x + xx, y + yy)
+        If foundStairs Then
+          stairFound = True
+        End If
+      Next
+    Next
+    ForegroundColor = ConsoleColor.Yellow
+    SetCursorPosition(x, y + 1)
+    Write(QBChar(1))
+  End Sub
+
+  Private Function DrawRoomTile(room$(), x%, y%) As Boolean
+    Dim result = False
+    Dim output = room(y)(x)
+    Dim fg = ConsoleColor.DarkGray
+    Dim bg = ConsoleColor.Black
+    Select Case output
+      Case "["c : output = WALL_TOP_LEFT : fg = ConsoleColor.DarkYellow
+      Case "]"c : output = WALL_TOP_RIGHT : fg = ConsoleColor.DarkYellow
+      Case "-"c : output = WALL_HORIZONTAL : fg = ConsoleColor.DarkYellow
+      Case "|"c : output = WALL_VERTICAL : fg = ConsoleColor.DarkYellow
+      Case "{"c : output = WALL_BOTTOM_LEFT : fg = ConsoleColor.DarkYellow
+      Case "}"c : output = WALL_BOTTOM_RIGHT : fg = ConsoleColor.DarkYellow
+      Case "+"c : output = WALL_DOOR : fg = ConsoleColor.DarkYellow
+      Case "="c
+        output = " "c
+        fg = ConsoleColor.Black : bg = ConsoleColor.Green
+        result = True
+      Case "@"c : output = QBChar(1) : fg = ConsoleColor.Yellow
+      Case "!"c : output = QBChar(173) : fg = ConsoleColor.DarkMagenta
+      Case "%"c : output = QBChar(4) : fg = ConsoleColor.Magenta
+      Case "$"c : output = QBChar(15) : fg = ConsoleColor.Yellow
+      Case "^"c : output = QBChar(24) : fg = ConsoleColor.DarkMagenta
+      Case "\"c : output = QBChar(8) : fg = ConsoleColor.DarkMagenta
+      Case "*"c : output = QBChar(5) : fg = ConsoleColor.Red
+      Case "#"c : output = TUNNEL
+      Case "@"c, "."c : output = "."c : fg = ConsoleColor.DarkGreen
+      Case "0"c : output = QBChar(248) : fg = ConsoleColor.DarkMagenta
+      Case "9"c : output = QBChar(13) : fg = ConsoleColor.DarkMagenta
+      Case Else
+    End Select
+    If BackgroundColor <> bg Then
+      BackgroundColor = bg
+    End If
+    If ForegroundColor <> fg Then
+      ForegroundColor = fg
+    End If
+    SetCursorPosition(x, y + 1) : Write(output)
+    Return result
   End Function
 
 End Module
