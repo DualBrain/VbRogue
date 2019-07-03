45511bc3248dfce8ce0f934f4dcb3e16458e8281
Cory Smith <corysmith@addressof.com>
2019-07-02 22:09:31 -0500

    Fixed issue with upper/left tile from hole showing green background when hero on that tile. Fixed issue hero not able to move into start tile. Added s key for search. Added number accumulator to handle repeated actions. Added Q to quit the game. Fixed issues with "exploring" regarding what shows and when. Fixed issue with "lit" room being lit as soon as you step into the doorway.

--- Rogue/Rogue/GameData.vb
+++ Rogue/Rogue/GameData.vb
@@ -7,6 +7,8 @@
     Hero ' On level start, replace with Floor.
     Hole
     Door
+    SecretVertical
+    SecretHorizontal
     WallHorizontal
     WallVertical
     WallTopLeft
@@ -35,10 +37,18 @@
 
       ' Is the tile "pass through"...
       Select Case type
-        Case TileType.Door, TileType.Tunnel, TileType.Floor, TileType.Hole
+        Case TileType.Hero, TileType.Door, TileType.Tunnel, TileType.Floor, TileType.Hole
           Me.PassThrough = True
         Case Else
           Me.PassThrough = False
+      End Select
+
+      ' Is the tile "secret"...
+      Select Case type
+        Case TileType.SecretHorizontal, TileType.SecretVertical
+          Me.Secret = True
+        Case Else
+          Me.Secret = False
       End Select
 
     End Sub
@@ -47,6 +57,45 @@
     Public ReadOnly Property PassThrough As Boolean
     Public ReadOnly Property Explored As Boolean
     Public ReadOnly Property HeroStart As Boolean
+    Public ReadOnly Property Secret As Boolean
+    Private Property SearchCount As Integer
+
+    Public Function FoundSecret(forced As Boolean) As Boolean
+
+      If Me.Secret Then
+
+        Me.SearchCount += 1
+
+        Dim probMiss As Double
+
+        Select Case Me.SearchCount
+          Case 1 : probMiss = 0.8
+          Case 2 : probMiss = 0.64
+          Case 3 : probMiss = 0.512
+          Case 4 : probMiss = 0.41
+          Case 5 : probMiss = 0.328
+          Case 6 : probMiss = 0.262
+          Case 7 : probMiss = 0.21
+          Case 8 : probMiss = 0.168
+          Case 9 : probMiss = 0.134
+          Case 10, 11 : probMiss = 0.107
+          Case 12, 13, 14 : probMiss = 0.069
+          Case Else : probMiss = 0.035
+        End Select
+
+        Dim v = SearchRandomizer.NextDouble
+
+        If forced OrElse v > probMiss Then
+          Me._Secret = False
+          Me._PassThrough = True
+          Return True
+        End If
+
+      End If
+
+      Return False
+
+    End Function
 
     Public Overrides Function ToString() As String
       Select Case Me.Type
@@ -61,6 +110,18 @@
         Case TileType.WallBottomRight : Return "╝"
         Case TileType.Door : Return "╬"
         Case TileType.Hole : Return "≡"
+        Case TileType.SecretHorizontal
+          If Me.Secret Then
+            Return "═"
+          Else
+            Return "╬"
+          End If
+        Case TileType.SecretVertical
+          If Me.Secret Then
+            Return "║"
+          Else
+            Return "╬"
+          End If
         Case Else
           Throw New Exception("Unknown tile type.")
       End Select
@@ -82,6 +143,8 @@
       Case "+"c : Return TileType.Door
       Case "="c : Return TileType.Hole
       Case "@"c : Return TileType.Hero
+      Case "/"c : Return TileType.SecretHorizontal
+      Case "\"c : Return TileType.SecretVertical
       Case Else
         Throw New Exception("Unknown tile type.")
     End Select
--- Rogue/Rogue/Program.vb
+++ Rogue/Rogue/Program.vb
@@ -9,6 +9,8 @@ Imports QB
 
 Module Program
 
+  Friend SearchRandomizer As New Random()
+
   'Sub Main(args As String())
   '  ' The following is from a sample by Lucian... 
   '  ' so I'll take it as the gospel. ;-)
@@ -74,6 +76,17 @@ Module Program
   Private m_levels As List(Of Level)
   Private m_level As Integer
 
+  Private m_holeX% = -1
+  Private m_holeY% = -1
+  Private m_holeFound As Boolean = False
+
+  Private m_heroX% = -1
+  Private m_heroY% = -1
+
+  Private m_moveCount% = 0
+
+  Private m_accumulator$
+
   'Private Async Function MainAsync(args As String()) As Task
   Public Sub Main(args As String())
 
@@ -118,14 +131,7 @@ Module Program
 
       ' Holds the current location of the "stairs" (aka hole).
 
-      Dim holeX = -1
-      Dim holeY = -1
-      Dim holeFound = False
-
-      Dim heroX = -1
-      Dim heroY = -1
-
-      InitializeLevel(holeX, holeY, holeFound, heroX, heroY)
+      InitializeLevel()
 
       ' "Game Loop"
 
@@ -139,15 +145,15 @@ Module Program
 
         Threading.Thread.Sleep(1)
 
-        If holeFound AndAlso
+        If m_holeFound AndAlso
            cycles > 250 AndAlso
-           (holeY > -1 AndAlso holeX > -1) AndAlso
-           Not (heroY = holeY AndAlso heroX = holeX) Then
+           (m_holeY > -1 AndAlso m_holeX > -1) AndAlso
+           Not (m_heroY = m_holeY AndAlso m_heroX = m_holeX) Then
           cycles = 0
           ' Draw stairway...
           ForegroundColor = ConsoleColor.Black
           BackgroundColor = ConsoleColor.Green
-          SetCursorPosition(holeX, holeY + 1)
+          SetCursorPosition(m_holeX, m_holeY + 1)
           If alt Then
             Write(" ")
           Else
@@ -158,50 +164,186 @@ Module Program
         End If
 
         If KeyAvailable Then
+
           Dim key = ReadKey(True)
+
+          Dim isAction = False
+
           Select Case key.Key
+            Case ConsoleKey.D0 : Accumulate("0"c)
+            Case ConsoleKey.D1 : Accumulate("1"c)
+            Case ConsoleKey.D2 : Accumulate("2"c)
+            Case ConsoleKey.D3 : Accumulate("3"c)
+            Case ConsoleKey.D4 : Accumulate("4"c)
+            Case ConsoleKey.D5 : Accumulate("5"c)
+            Case ConsoleKey.D6 : Accumulate("6"c)
+            Case ConsoleKey.D7 : Accumulate("7"c)
+            Case ConsoleKey.D8 : Accumulate("8"c)
+            Case ConsoleKey.D9 : Accumulate("9"c)
             Case ConsoleKey.Insert
-              If heroX = holeX AndAlso
-                 heroY = holeY Then
+              If m_heroX = m_holeX AndAlso
+                 m_heroY = m_holeY Then
                 m_level += 1
                 If m_level > m_levels.Count - 1 Then
-                  Return
+                  Exit Do
                 End If
-                InitializeLevel(holeX, holeY, holeFound, heroX, heroY)
+                InitializeLevel()
               End If
-            Case ConsoleKey.UpArrow
-              If CanMove(heroX, heroY - 1) Then
-                DrawRoomTile(heroX, heroY)
-                heroY -= 1
-                PlaceHero(heroX, heroY, holeFound)
+            Case ConsoleKey.Q
+              'TODO: Need to determine if uppercase Q.
+              If key.Modifiers = ConsoleModifiers.Shift Then
+                SetCursorPosition(0, 0)
+                ForegroundColor = ConsoleColor.Gray
+                BackgroundColor = ConsoleColor.Black
+                Write("Do you wish to end your quest now (Yes/No) ?")
+                ForegroundColor = ConsoleColor.Black
+                BackgroundColor = ConsoleColor.Gray
+                SetCursorPosition(35, 0)
+                Write("Y")
+                SetCursorPosition(39, 0)
+                Write("N")
+                Dim quit = False
+                Do
+                  If KeyAvailable Then
+                    key = ReadKey(True)
+                    Select Case key.Key
+                      Case ConsoleKey.Y
+                        quit = True
+                        Exit Do
+                      Case ConsoleKey.N
+                        SetCursorPosition(0, 0)
+                        ForegroundColor = ConsoleColor.Gray
+                        BackgroundColor = ConsoleColor.Black
+                        Write("                                            ")
+                        Exit Do
+                      Case Else
+                    End Select
+                  End If
+                Loop
+                If quit Then
+                  Exit Do
+                End If
               End If
-            Case ConsoleKey.DownArrow
-              If CanMove(heroX, heroY + 1) Then
-                DrawRoomTile(heroX, heroY)
-                heroY += 1
-                PlaceHero(heroX, heroY, holeFound)
-              End If
-            Case ConsoleKey.LeftArrow
-              If CanMove(heroX - 1, heroY) Then
-                DrawRoomTile(heroX, heroY)
-                heroX -= 1
-                PlaceHero(heroX, heroY, holeFound)
-              End If
-            Case ConsoleKey.RightArrow
-              If CanMove(heroX + 1, heroY) Then
-                DrawRoomTile(heroX, heroY)
-                heroX += 1
-                PlaceHero(heroX, heroY, holeFound)
-              End If
-            Case ConsoleKey.Spacebar, ConsoleKey.Escape
-              Exit Do
+
+            Case ConsoleKey.UpArrow,
+                 ConsoleKey.DownArrow,
+                 ConsoleKey.LeftArrow,
+                 ConsoleKey.RightArrow,
+                 ConsoleKey.S
+
+              isAction = True
+
             Case Else
+              ' Do nothing...
           End Select
+
+          If isAction Then
+
+            Dim repeatCount = 1
+            If m_accumulator <> "" Then
+              repeatCount = CInt(m_accumulator)
+            End If
+
+            For i = repeatCount To 1 Step -1
+
+              Select Case key.Key
+                Case ConsoleKey.UpArrow
+                  If CanMove(m_heroX, m_heroY - 1) Then
+                    DrawRoomTile(m_heroX, m_heroY)
+                    m_heroY -= 1
+                    PlaceHero()
+                  Else
+                    Exit For
+                  End If
+                Case ConsoleKey.DownArrow
+                  If CanMove(m_heroX, m_heroY + 1) Then
+                    DrawRoomTile(m_heroX, m_heroY)
+                    m_heroY += 1
+                    PlaceHero()
+                  Else
+                    Exit For
+                  End If
+                Case ConsoleKey.LeftArrow
+                  If CanMove(m_heroX - 1, m_heroY) Then
+                    DrawRoomTile(m_heroX, m_heroY)
+                    m_heroX -= 1
+                    PlaceHero()
+                  Else
+                    Exit For
+                  End If
+                Case ConsoleKey.RightArrow
+                  If CanMove(m_heroX + 1, m_heroY) Then
+                    DrawRoomTile(m_heroX, m_heroY)
+                    m_heroX += 1
+                    PlaceHero()
+                  Else
+                    Exit For
+                  End If
+                Case ConsoleKey.S
+                  If PerformSearch() Then
+                    Exit For
+                  End If
+                Case Else
+              End Select
+
+              m_moveCount += 1
+
+              If m_accumulator <> "" Then
+                m_accumulator = i.ToString
+                DrawAccumulator()
+              End If
+
+            Next
+
+            ClearAccumulator()
+
+          End If
+
         End If
 
         cycles += 1
 
+        SetCursorPosition(0, 0)
+        BackgroundColor = ConsoleColor.Black
+        ForegroundColor = ConsoleColor.Cyan
+        Write($"{m_moveCount}     ")
+
       Loop
+
+      ResetColor()
+      Clear()
+
+      SetCursorPosition(0, 0)
+      Write($"You quit with 0 gold pieces")
+
+      SetCursorPosition(0, 24)
+      Write($"[Press Enter to see rankings]")
+
+      Do
+        If KeyAvailable Then
+          Dim key = ReadKey(True)
+          Select Case key.Key
+            Case ConsoleKey.Enter
+              Exit Do
+            Case Else
+          End Select
+        End If
+      Loop
+
+      Clear()
+
+      ForegroundColor = ConsoleColor.White
+      WriteLine("Guildmaster's Hall of Fame:")
+      WriteLine()
+      ForegroundColor = ConsoleColor.Yellow
+      WriteLine("Gold")
+      WriteLine()
+      ResetColor()
+      WriteLine("TODO: Display hall-of-fame list.")
+      WriteLine()
+      WriteLine()
+      WriteLine()
+      WriteLine()
 
 #Region "Reset Console Window"
 
@@ -234,6 +376,45 @@ Module Program
 #End Region
 
   End Sub
+
+  Private Sub Accumulate(c As Char)
+    If m_accumulator Is Nothing Then
+      m_accumulator = c
+    ElseIf m_accumulator?.Length < 4 Then
+      m_accumulator &= c
+    End If
+    DrawAccumulator()
+  End Sub
+
+  Private Sub DrawAccumulator()
+    BackgroundColor = ConsoleColor.Black
+    ForegroundColor = ConsoleColor.Gray
+    SetCursorPosition(76, 23)
+    Write(If(m_accumulator, "").PadRight(4))
+  End Sub
+
+  Private Sub ClearAccumulator()
+    m_accumulator = Nothing
+    DrawAccumulator()
+  End Sub
+
+  Private Function PerformSearch() As Boolean
+
+    Dim result = False
+
+    For yy = -1 To 1
+      For xx = -1 To 1
+        Dim tile = m_levels(m_level).Map(m_heroY + yy, m_heroX + xx)
+        If tile.FoundSecret(False) Then
+          DrawRoomTile(m_heroX + xx, m_heroY + yy)
+          result = True
+        End If
+      Next
+    Next
+
+    Return result
+
+  End Function
 
   Private Function LoadDungeon(path As String) As List(Of Level)
 
@@ -278,29 +459,38 @@ Module Program
 
   End Function
 
-  Private Sub InitializeLevel(ByRef holeX As Integer, ByRef holeY As Integer, ByRef holeFound As Boolean, ByRef heroX As Integer, ByRef heroY As Integer)
+  Private Sub InitializeLevel()
 
-    holeX = -1
-    holeY = -1
-    holeFound = False
+    m_accumulator = Nothing
+    m_moveCount = 0
 
-    heroX = -1
-    heroY = -1
+    m_holeX = -1
+    m_holeY = -1
+    m_holeFound = False
 
+    m_heroX = -1
+    m_heroY = -1
+
+    BackgroundColor = ConsoleColor.Black
     Clear()
     WriteLine()
     For y = 0 To 20
       For x = 0 To 79
         Dim tile = m_levels(m_level).Map(y, x)
         If tile.HeroStart Then
-          heroX = x : heroY = y
+          m_heroX = x : m_heroY = y
         ElseIf tile.Type = TileType.Hole Then
-          holeX = x : holeY = y
+          m_holeX = x : m_holeY = y
         End If
       Next
     Next
 
-    PlaceHero(heroX, heroY, holeFound)
+    Dim zone = DetermineZone(m_heroX, m_heroY)
+    Dim lit = If(zone > -1, m_levels(m_level).Lights(zone), False)
+    If lit Then
+      DrawRoom(m_heroX, m_heroY)
+    End If
+    PlaceHero()
 
   End Sub
 
@@ -350,52 +540,140 @@ Module Program
 
   End Function
 
-  Private Sub PlaceHero(x%, y%, ByRef stairFound As Boolean)
+  Private Class Coord
+    Public Sub New(x As Integer, y As Integer)
+      Me.X = x
+      Me.Y = y
+    End Sub
+    Public ReadOnly Property X As Integer
+    Public ReadOnly Property Y As Integer
+  End Class
 
-    Dim zone = DetermineZone(x, y)
+  Private Sub PlaceHero()
+
+    Dim zone = DetermineZone(m_heroX, m_heroY)
     Dim lit = If(zone > -1, m_levels(m_level).Lights(zone), False)
-    Dim floor = If(zone > -1, m_levels(m_level).Map(y, x).Type = TileType.Floor, False)
+    Dim floor = If(zone > -1, m_levels(m_level).Map(m_heroY, m_heroX).Type = TileType.Floor, False)
 
-    If lit AndAlso floor Then
+    'If lit AndAlso floor Then
 
-      Dim xx = x
-      Dim yy = y
-      Do
-        If m_levels(m_level).Map(yy, xx - 1).Type = TileType.Floor Then
-          xx -= 1
+    '  Dim xx = m_heroX
+    '  Dim yy = m_heroY
+    '  Do
+    '    If m_levels(m_level).Map(yy, xx - 1).Type = TileType.Floor Then
+    '      xx -= 1
+    '    Else
+    '      Exit Do
+    '    End If
+    '  Loop
+    '  Do
+    '    If m_levels(m_level).Map(yy - 1, xx).Type = TileType.Floor Then
+    '      yy -= 1
+    '    Else
+    '      Exit Do
+    '    End If
+    '  Loop
+
+    'Else
+
+    Dim tile = m_levels(m_level).Map(m_heroY, m_heroX)
+
+    Select Case tile.Type
+      Case TileType.Floor
+        For yy = -1 To 1
+          For xx = -1 To 1
+            Dim foundStairs = DrawRoomTile(m_heroX + xx, m_heroY + yy)
+            If foundStairs Then
+              m_holeFound = True
+            End If
+          Next
+        Next
+      Case TileType.Tunnel
+        For yy = -1 To 1
+          For xx = -1 To 1
+            Dim target = m_levels(m_level).Map(m_heroY + yy, m_heroX + xx)
+            Select Case target.Type
+              Case TileType.Tunnel, TileType.Door
+                DrawRoomTile(m_heroX + xx, m_heroY + yy)
+              Case Else
+            End Select
+          Next
+        Next
+      Case TileType.Door, TileType.SecretHorizontal, TileType.SecretVertical
+        If lit Then
+          ' In the doorway, determine the floor to the left, right, top or bottom.
+          ' Once found, use "paint" routine above...
+          Dim coords = {New Coord(0, -1),
+                        New Coord(1, 0),
+                        New Coord(0, 1),
+                        New Coord(-1, 0)}
+          For Each coord In coords
+            Dim target = m_levels(m_level).Map(m_heroY + coord.Y, m_heroX + coord.X)
+            If target.Type = TileType.Floor Then
+              ' found it...
+              DrawRoom(m_heroX + coord.X, m_heroY + coord.Y)
+              m_levels(m_level).Lights(zone) = False
+              Exit For
+            End If
+          Next
         Else
-          Exit Do
+          For yy = -1 To 1
+            For xx = -1 To 1
+              Dim foundStairs = DrawRoomTile(m_heroX + xx, m_heroY + yy)
+              If foundStairs Then
+                m_holeFound = True
+              End If
+            Next
+          Next
         End If
-      Loop
+      Case Else
+    End Select
+
+    'End If
+
+    If m_levels(m_level).Map(m_heroY, m_heroX).Type = TileType.Hole Then
+      BackgroundColor = ConsoleColor.Green
+    Else
+      BackgroundColor = ConsoleColor.Black
+    End If
+    ForegroundColor = ConsoleColor.Yellow
+    SetCursorPosition(m_heroX, m_heroY + 1)
+    Write("☺")
+
+  End Sub
+
+  Private Sub DrawRoom(x As Integer, y As Integer)
+
+    ' Find the top / left floor tile in this room.
+
+    Dim xx = x
+    Dim yy = y
+    Do
+      If m_levels(m_level).Map(yy, xx - 1).Type = TileType.Floor Then
+        xx -= 1
+      Else
+        Exit Do
+      End If
+    Loop
+    Do
+      If m_levels(m_level).Map(yy - 1, xx).Type = TileType.Floor Then
+        yy -= 1
+      Else
+        Exit Do
+      End If
+    Loop
+
+    Dim xxx = xx - 1
+    Dim yyy = yy - 1
+
+    Do
+
       Do
-        If m_levels(m_level).Map(yy - 1, xx).Type = TileType.Floor Then
-          yy -= 1
-        Else
-          Exit Do
+        Dim foundStairs = DrawRoomTile(xxx, yyy)
+        If foundStairs Then
+          m_holeFound = True
         End If
-      Loop
-
-      Dim xxx = xx - 1
-      Dim yyy = yy - 1
-
-      Do
-
-        Do
-
-          Dim foundStairs = DrawRoomTile(xxx, yyy)
-          If foundStairs Then
-            stairFound = True
-          End If
-          xxx += 1
-          Select Case m_levels(m_level).Map(yyy, xxx).Type
-            Case TileType.Void, TileType.Tunnel
-              Exit Do
-            Case Else
-          End Select
-        Loop
-        xxx = xx - 1
-        yyy += 1
-        If yyy > 20 Then Exit Do
+        xxx += 1
         Select Case m_levels(m_level).Map(yyy, xxx).Type
           Case TileType.Void, TileType.Tunnel
             Exit Do
@@ -403,22 +681,16 @@ Module Program
         End Select
       Loop
 
-      m_levels(m_level).Lights(zone) = False
+      xxx = xx - 1
+      yyy += 1
+      If yyy > 20 Then Exit Do
+      Select Case m_levels(m_level).Map(yyy, xxx).Type
+        Case TileType.Void, TileType.Tunnel
+          Exit Do
+        Case Else
+      End Select
 
-    Else
-      For yy = -1 To 1
-        For xx = -1 To 1
-          Dim foundStairs = DrawRoomTile(x + xx, y + yy)
-          If foundStairs Then
-            stairFound = True
-          End If
-        Next
-      Next
-    End If
-
-    ForegroundColor = ConsoleColor.Yellow
-    SetCursorPosition(x, y + 1)
-    Write("☺")
+    Loop
 
   End Sub
 
@@ -435,6 +707,8 @@ Module Program
       Case TileType.WallVertical : fg = ConsoleColor.DarkYellow
       Case TileType.WallBottomLeft : fg = ConsoleColor.DarkYellow
       Case TileType.WallBottomRight : fg = ConsoleColor.DarkYellow
+      Case TileType.SecretHorizontal : fg = ConsoleColor.DarkYellow
+      Case TileType.SecretVertical : fg = ConsoleColor.DarkYellow
       Case TileType.Door : fg = ConsoleColor.DarkYellow
       Case TileType.Hole
         fg = ConsoleColor.Black : bg = ConsoleColor.Green
